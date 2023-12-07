af60ba58adedbd83d1079d4fff64a557dbbfac4b
Maria Biserica <maria.biserica@student.unitbv.ro>
2023-12-07 21:17:59 +0200

    Implemented King class.

--- MyGame/GameLib/King.cpp
+++ MyGame/GameLib/King.cpp
@@ -1,1 +1,14 @@
 #include "King.h"
+#include "Board.h"
+
+
+King::King(EPieceRole role)
+    : Piece(EPieceType::King, role)
+{
+
+}
+
+bool King::CanMove(Position startPos, Position endPos, const Board& board)
+{
+    return false;
+}
--- MyGame/GameLib/King.h
+++ MyGame/GameLib/King.h
@@ -1,5 +1,13 @@
 #pragma once
-class King
+
+#include "Piece.h"
+
+class Board;
+
+class King : public Piece
 {
+public:
+	King(EPieceRole role);
+	bool CanMove(Position startPos, Position endPos, const Board& board);
 };
 
