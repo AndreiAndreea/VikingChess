7e614299266cae185acc7623331d4dabbcb83cf8
Andreea Andrei <andreiandreea749@gmail.com>
2023-12-07 16:18:30 +0200

    Added an interfac e for hame and implemented the Game class.

--- MyGame/GameLib/Game.h
+++ MyGame/GameLib/Game.h
@@ -1,1 +1,16 @@
 #pragma once
+#include<iostream>
+#include "Board.h"
+#include "Player.h"
+#include "IGame.h"
+
+class Game : public IGame
+{
+
+public:
+
+	void Play() override;
+	EPlayer GetWinner() override;
+	bool IsGameOver() override;
+
+};
---
+++ MyGame/GameLib/IGame.h
@@ -0,0 +1,24 @@
+#pragma once
+
+enum class EPlayer
+{
+	Attacker,
+	Defender,
+	None
+};
+
+using IGamePtr = std::shared_ptr<class IGame>;
+
+class IGame
+{
+
+public:
+	static IGamePtr Produce();
+
+	virtual void Play() = 0;
+	virtual EPlayer GetWinner() = 0;
+	virtual bool IsGameOver() = 0;
+
+	virtual ~IGame() = default;
+};
+
