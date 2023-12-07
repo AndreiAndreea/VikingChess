ffb254af7b5eb5e73a2e4264442dd957a85559e9
Maria Biserica <maria.biserica@student.unitbv.ro>
2023-12-07 20:59:47 +0200

    Implemented Piece class.

--- MyGame/GameLib/Piece.cpp
+++ MyGame/GameLib/Piece.cpp
@@ -1,1 +1,17 @@
 #include "Piece.h"
+
+EPieceType Piece::GetType() const
+{
+	return m_type;
+}
+
+Piece::Piece(EPieceType type, EPieceRole role)
+{
+	m_type = type;
+	m_role = role;
+}
+
+EPieceRole Piece::GetRole() const
+{
+	return m_role;
+}
--- MyGame/GameLib/Piece.h
+++ MyGame/GameLib/Piece.h
@@ -1,5 +1,28 @@
- #pragma once
-class Piece
+#pragma once
+
+#include "IPiece.h"
+
+#include <string>
+#include <memory>
+#include <cmath>
+
+class Board;
+
+class Piece : public IPiece
 {
+public:
+	Piece(EPieceType type, EPieceRole role);
+
+	// IPiece implementation
+	EPieceRole GetRole() const override;
+	EPieceType GetType() const override;
+
+private:
+	EPieceType m_type;
+	EPieceRole m_role;
 };
 
+using PiecePtr = std::shared_ptr<Piece>;
+
+//role - defenter or attacker
+//type - warrior or king
