65f6bce55f986dbee680929a76636c4072025ad0
Maria Biserica <maria.biserica@student.unitbv.ro>
2023-12-08 10:26:44 +0200

    Added piece role and type enums.

---
+++ MyGame/GameLib/EPieceType.h
@@ -0,0 +1,6 @@
+#pragma once
+enum class EPieceType
+{
+	Warrior,
+	King
+};
--- MyGame/GameLib/IPiece.h
+++ MyGame/GameLib/IPiece.h
@@ -1,23 +1,15 @@
 #pragma once
 
 #include <utility>
+#include <vector>
+
+#include "EPieceRole.h"
+#include "EPieceType.h"
 
 class Board;
 
-enum class EPieceRole
-{
-	Attacker,
-	Defender
-};
-
-
-enum class EPieceType
-{
-	Warrior,
-	King
-};
-
 using Position = std::pair<int, int>;
+using PositionList = std::vector<Position>;
 
 class IPiece
 {
@@ -25,6 +17,8 @@ public:
 	virtual EPieceRole GetRole() const = 0;
 	virtual EPieceType GetType() const = 0;
 	virtual bool CanMove(Position startPos, Position endPos, const Board& board) = 0;
+	virtual PositionList GetPossibleMoves(Position piecePos, const Board& board) = 0;
+	virtual bool Is(EPieceType type, EPieceRole role) const = 0;
 
 	virtual ~IPiece() = default;
 };
