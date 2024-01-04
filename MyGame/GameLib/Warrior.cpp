a52452b6be2e0fc84a320aa32cc474507ce6bc73
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-01-04 13:49:07 +0200

    Simplified GetPossibleMoves method for Warrior.

--- MyGame/GameLib/Warrior.cpp
+++ MyGame/GameLib/Warrior.cpp
@@ -24,22 +24,20 @@ PositionList Warrior::GetPossibleMoves(Position piecePos, const Board& board)
 	PositionList possibleMoves;
 
 	// check left
-	for (int j = piecePos.second - 1; j > 0 && board.GetBoard()[piecePos.first][j] == nullptr; j--)
-			possibleMoves.push_back(Position(piecePos.first, j));
-
+	for (int j = piecePos.second - 1; j >= 1 && board.GetBoard()[piecePos.first][j] == nullptr; j--)
+		possibleMoves.push_back(Position(piecePos.first, j));
 
 	// check right
 	for (int j = piecePos.second + 1; j <= 11 && board.GetBoard()[piecePos.first][j] == nullptr; j++)
-	
-			possibleMoves.push_back(Position(piecePos.first, j));
+		possibleMoves.push_back(Position(piecePos.first, j));
 
 	// check up
-	for (int i = piecePos.first - 1; i <=1 && board.GetBoard()[i][piecePos.second] == nullptr; i--)
-			possibleMoves.push_back(Position(i, piecePos.second));
+	for (int i = piecePos.first - 1; i >= 1 && board.GetBoard()[i][piecePos.second] == nullptr; i--)
+		possibleMoves.push_back(Position(i, piecePos.second));
 
-	//check down
-	for (int i = piecePos.first + 1; i >= 11 && board.GetBoard()[i][piecePos.second] == nullptr; i++)
-			possibleMoves.push_back(Position(i, piecePos.second));
+	// check down
+	for (int i = piecePos.first + 1; i <= 11 && board.GetBoard()[i][piecePos.second] == nullptr; i++)
+		possibleMoves.push_back(Position(i, piecePos.second));
 
 	return possibleMoves;
 }
