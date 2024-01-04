6a789c9aa56eede99f8347b30c5807c3485eee1a
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-01-04 15:39:33 +0200

    Started implementing special sandwich capture case - top left corner.

--- MyGame/GameLib/Board.cpp
+++ MyGame/GameLib/Board.cpp
@@ -216,7 +216,17 @@ bool Board::MakeMove(Position startPos, Position endPos)
 			{
 				SetPieceToNullptr(Position(endPos.first + 1, endPos.second));
 			}
+			// ---------------------------------
 
+			// Check special case sandwich - trap a piece in a corner
+			// Top left corner - trap from bottom
+			if (endPos.first == 2 && endPos.second == 1 &&
+				m_board[endPos.first - 1][endPos.second] && m_board[endPos.first - 1][endPos.second + 1] &&
+				IsOpposite(m_board[endPos.first - 1][endPos.second], piece->GetRole(), { EPieceType::Warrior,EPieceType::King }) &&
+				!IsOpposite(m_board[endPos.first - 1][endPos.second + 1], piece->GetRole(), { EPieceType::Warrior,EPieceType::King }))
+			{
+				SetPieceToNullptr(Position(endPos.first - 1, endPos.second));
+			}
 		}
 		return true;
 	}
