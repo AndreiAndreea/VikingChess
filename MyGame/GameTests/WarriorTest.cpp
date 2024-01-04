7bec47ffa8a81cde2cacc613247ec8a0631e2e98
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-01-04 18:55:33 +0200

    Started implementing GetPossibleMoves method for King.

--- MyGame/GameTests/WarriorTest.cpp
+++ MyGame/GameTests/WarriorTest.cpp
@@ -31,3 +31,47 @@ TEST_F(WarriorTest, InvalidMoveUpwards) {
 
 	EXPECT_FALSE(canMove);
 }
+
+TEST_F(WarriorTest, InvalidMoveDiagonally) {
+	Board board;
+
+	Position startPos(6, 1);
+	Position endPos(5, 2);
+	board.SetPiece(startPos, EPieceRole::Attacker, EPieceType::Warrior);
+	bool canMove = warrior->CanMove(startPos, endPos, board);
+
+	EXPECT_FALSE(canMove);
+}
+
+TEST_F(WarriorTest, InvalidMoveDownwards) {
+	Board board;
+
+	Position startPos(2, 6);
+	Position endPos(4, 6);
+	board.SetPiece(startPos, EPieceRole::Attacker, EPieceType::Warrior);
+	bool canMove = warrior->CanMove(startPos, endPos, board);
+
+	EXPECT_FALSE(canMove);
+}
+
+TEST_F(WarriorTest, ValidMoveUpwards) {
+	Board board;
+
+	Position startPos(11, 7);
+	Position endPos(8, 7);
+	board.SetPiece(startPos, EPieceRole::Attacker, EPieceType::Warrior);
+	bool canMove = warrior->CanMove(startPos, endPos, board);
+
+	EXPECT_TRUE(canMove);
+}
+
+TEST_F(WarriorTest, ValidMoveDownwards) {
+	Board board;
+
+	Position startPos(6, 10);
+	Position endPos(11, 10);
+	board.SetPiece(startPos, EPieceRole::Attacker, EPieceType::Warrior);
+	bool canMove = warrior->CanMove(startPos, endPos, board);
+
+	EXPECT_TRUE(canMove);
+}
