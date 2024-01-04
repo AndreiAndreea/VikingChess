5f72228f6924b917b09a13e0997861f30c0d594b
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-01-04 18:38:18 +0200

    Added check for illegal move to enter a sandwich and modified MakeMove method for Warrior.

--- MyGame/GameLib/Warrior.cpp
+++ MyGame/GameLib/Warrior.cpp
@@ -18,6 +18,26 @@ bool Warrior::CanMove(Position startPos, Position endPos, const Board& board)
 	return false;
 }
 
+bool Warrior::IsSandwich(Position endPos, const Board& board)
+{
+	// Surrounded vertically
+	if (endPos.first - 1 >= 1 && endPos.first + 1 <= 11 &&
+		board.GetPiece(Position(endPos.first - 1, endPos.second)) &&
+		board.GetPiece(Position(endPos.first + 1, endPos.second)) &&
+		IsOpposite(board.GetPiece(Position(endPos.first - 1, endPos.second))->GetRole(), { EPieceType::Warrior,EPieceType::King }) &&
+		IsOpposite(board.GetPiece(Position(endPos.first + 1, endPos.second))->GetRole(), { EPieceType::Warrior,EPieceType::King }))
+		return true;
+	else // Surrounded horizontally
+		if (endPos.second - 1 >= 1 && endPos.second + 1 <= 11 &&
+			board.GetPiece(Position(endPos.first, endPos.second - 1)) &&
+			board.GetPiece(Position(endPos.first, endPos.second + 1)) &&
+			IsOpposite(board.GetPiece(Position(endPos.first, endPos.second - 1))->GetRole(), { EPieceType::Warrior,EPieceType::King }) &&
+			IsOpposite(board.GetPiece(Position(endPos.first, endPos.second + 1))->GetRole(), { EPieceType::Warrior,EPieceType::King }))
+			return true;
+
+	return false;
+}
+
 
 PositionList Warrior::GetPossibleMoves(Position piecePos, const Board& board)
 {
@@ -25,19 +45,23 @@ PositionList Warrior::GetPossibleMoves(Position piecePos, const Board& board)
 
 	// check left
 	for (int j = piecePos.second - 1; j >= 1 && board.GetBoard()[piecePos.first][j] == nullptr; j--)
-		possibleMoves.push_back(Position(piecePos.first, j));
+		if (!IsSandwich(Position(piecePos.first, j), board))
+			possibleMoves.push_back(Position(piecePos.first, j));
 
 	// check right
 	for (int j = piecePos.second + 1; j <= 11 && board.GetBoard()[piecePos.first][j] == nullptr; j++)
-		possibleMoves.push_back(Position(piecePos.first, j));
+		if (!IsSandwich(Position(piecePos.first, j), board))
+			possibleMoves.push_back(Position(piecePos.first, j));
 
 	// check up
 	for (int i = piecePos.first - 1; i >= 1 && board.GetBoard()[i][piecePos.second] == nullptr; i--)
-		possibleMoves.push_back(Position(i, piecePos.second));
+		if (!IsSandwich(Position(i, piecePos.second), board))
+			possibleMoves.push_back(Position(i, piecePos.second));
 
 	// check down
 	for (int i = piecePos.first + 1; i <= 11 && board.GetBoard()[i][piecePos.second] == nullptr; i++)
-		possibleMoves.push_back(Position(i, piecePos.second));
+		if (!IsSandwich(Position(i, piecePos.second), board))
+			possibleMoves.push_back(Position(i, piecePos.second));
 
 	return possibleMoves;
 }
--- MyGame/GameLib/Warrior.h
+++ MyGame/GameLib/Warrior.h
@@ -10,6 +10,7 @@ class Warrior : public Piece
 public:
 	Warrior(EPieceRole role);
 	bool CanMove(Position startPos, Position endPos, const Board& board);
+	bool IsSandwich(Position endPos, const Board& board);
 	PositionList GetPossibleMoves(Position piecePos, const Board& board);
 };
 
