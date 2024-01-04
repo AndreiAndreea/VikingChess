719c4042217f598d9973275795256774fc755bdc
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-01-04 15:46:00 +0200

    Finished implementing special sandwich capture case - top left corner.

--- MyGame/GameLib/Board.cpp
+++ MyGame/GameLib/Board.cpp
@@ -227,6 +227,14 @@ bool Board::MakeMove(Position startPos, Position endPos)
 			{
 				SetPieceToNullptr(Position(endPos.first - 1, endPos.second));
 			}
+			else // - trap from right
+				if (endPos.first == 1 && endPos.second == 2 &&
+					m_board[endPos.first][endPos.second - 1] && m_board[endPos.first + 1][endPos.second - 1] &&
+					IsOpposite(m_board[endPos.first][endPos.second - 1], piece->GetRole(), { EPieceType::Warrior,EPieceType::King }) &&
+					!IsOpposite(m_board[endPos.first + 1][endPos.second - 1], piece->GetRole(), { EPieceType::Warrior,EPieceType::King }))
+				{
+					SetPieceToNullptr(Position(endPos.first, endPos.second - 1));
+				}
 		}
 		return true;
 	}
