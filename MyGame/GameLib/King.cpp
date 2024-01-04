dd065ec974fecc72eb2d8f5bf06f789fa4739e2c
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-01-04 19:31:12 +0200

    Started implementing IsThreatened method in King to check move safety - down position open case.

--- MyGame/GameLib/King.cpp
+++ MyGame/GameLib/King.cpp
@@ -18,6 +18,22 @@ bool King::CanMove(Position startPos, Position endPos, const Board& board)
 	return false;
 }
 
+bool King::IsThreatened(Position piecePos, const Board& board)
+{
+	// The king is threatened if it is surrounded by 3 attackers
+	// Down position open
+	if (piecePos.second - 1 >= 1 && piecePos.first - 1 >= 1 && piecePos.second + 1 <= 11 &&
+		board.GetPiece(Position(piecePos.first, piecePos.second - 1)) &&
+		board.GetPiece(Position(piecePos.first - 1, piecePos.second)) &&
+		board.GetPiece(Position(piecePos.first, piecePos.second + 1)) &&
+		board.GetBoard()[piecePos.first][piecePos.second - 1]->Is(EPieceType::Warrior, EPieceRole::Attacker) &&
+		board.GetBoard()[piecePos.first - 1][piecePos.second]->Is(EPieceType::Warrior, EPieceRole::Attacker) &&
+		board.GetBoard()[piecePos.first][piecePos.second + 1]->Is(EPieceType::Warrior, EPieceRole::Attacker))
+		return true;	
+
+	return false;
+}
+
 
 PositionList King::GetPossibleMoves(Position piecePos, const Board& board)
 {
@@ -25,19 +41,23 @@ PositionList King::GetPossibleMoves(Position piecePos, const Board& board)
 
 	// check left
 	for (int j = piecePos.second - 1; j >= 1 && board.GetBoard()[piecePos.first][j] == nullptr; j--)
-		possibleMoves.push_back(Position(piecePos.first, j));
+		if (!IsThreatened(Position(piecePos.first, j), board))
+			possibleMoves.push_back(Position(piecePos.first, j));
 
 	// check right
 	for (int j = piecePos.second + 1; j <= 11 && board.GetBoard()[piecePos.first][j] == nullptr; j++)
-		possibleMoves.push_back(Position(piecePos.first, j));
+		if (!IsThreatened(Position(piecePos.first, j), board))
+			possibleMoves.push_back(Position(piecePos.first, j));
 
 	// check up
 	for (int i = piecePos.first - 1; i >= 1 && board.GetBoard()[i][piecePos.second] == nullptr; i--)
-		possibleMoves.push_back(Position(i, piecePos.second));
+		if (!IsThreatened(Position(i, piecePos.second), board))
+			possibleMoves.push_back(Position(i, piecePos.second));
 
 	// check down
 	for (int i = piecePos.first + 1; i <= 11 && board.GetBoard()[i][piecePos.second] == nullptr; i++)
-		possibleMoves.push_back(Position(i, piecePos.second));
+		if (!IsThreatened(Position(i, piecePos.second), board))
+			possibleMoves.push_back(Position(i, piecePos.second));
 
 	return possibleMoves;
 }
--- MyGame/GameLib/King.h
+++ MyGame/GameLib/King.h
@@ -8,8 +8,8 @@ class King : public Piece
 {
 public:
 	King(EPieceRole role);
-	PositionList GetPossibleMoves(Position piecePos, const Board& board);
-
 	bool CanMove(Position startPos, Position endPos, const Board& board);
+	bool IsThreatened(Position piecePos, const Board& board);
+	PositionList GetPossibleMoves(Position piecePos, const Board& board);
 };
 
